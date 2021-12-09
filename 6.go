aad25cd9d3d8770685065e319c27c03b3b0799e9
kalen795@gmail.com <ZeroCheats1331>
2021-12-10 00:16:40 +0300

    update 6.go

--- 6.go
+++ 6.go
@@ -3,20 +3,71 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	stopGoroutine()
+	stopGoroutineByDeadline()
 }
 
-func stopGoroutine() {
-	var num time.Duration
-	fmt.Println("Enter the number of seconds: ")
-	fmt.Scan(&num)
+func stopGoroutineByDeadline() {
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(3*time.Second))
+	defer cancel()
 
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("overslept")
+			}
+		}
+	}()
+}
+
+func stopGoroutineByTime() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	go func() {
+		select {
+		case <-time.After(10 * time.Second):
+			fmt.Println("overslept")
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
+		}
+	}()
+}
+
+func stopGoroutineByWaitGroup() {
+	chann := make(chan int)
+	wg := &sync.WaitGroup{}
+	go func() {
+		defer wg.Done()
+		wg.Add(1)
+		for {
+			select {
+			case <-chann:
+				return
+			}
+		}
+	}()
+}
+
+func stopGoroutineByClose() {
+	chann := make(chan int)
+	wg := &sync.WaitGroup{}
+	go func() {
+		defer wg.Done()
+		wg.Add(1)
+		close(chann)
+	}()
+}
+
+func stopGoroutineByContext() {
 	chann := make(chan int)
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	writer := func(ctx context.Context) <-chan int {
 		i := 1
@@ -24,25 +75,19 @@ func stopGoroutine() {
 			for {
 				select {
 				case <-ctx.Done():
-					close(chann)
 					return
 				case chann <- i:
 					i++
 					fmt.Println("working")
 				}
-
-				time.Sleep(500 * time.Millisecond)
 			}
 		}()
 		return chann
 	}
 
-	go func() {
-		time.Sleep(num * time.Second)
-		cancel()
-	}()
-
 	for i := range writer(ctx) {
-		fmt.Println(i)
+		if i == 3 {
+			break
+		}
 	}
 }
